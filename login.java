342c6029af94788c97ed0ef409a3f0fcefe8435b
rahulmiryala07 <151643892+rahulmiryala07@users.noreply.github.com>
2025-11-30 23:26:21 +0530

    Update login.java

--- login.java
+++ login.java
@@ -1,1 +1,8 @@
-Development notes
+package hello;
+public class login {
+  public static void main(String[] args) {
+    System.out.println("Hello from Pipeline CI!");
+    System.out.println("Hello!");
+    System.out.println("Hello!");
+  }
+}
